4f75a1e8db0449e9bbc7d8a77ad3dfa840059340
mlibersat <100230347+mlibersat@users.noreply.github.com>
2023-07-17 11:34:06 -0500

    updates

---
+++ Junk Drawer/testUAT.js
@@ -0,0 +1,14 @@
+const { rte1, rte2 } = components;
+console.log(rte1.data.text);
+
+rte1.updateData({
+  text: '<p>This is my happy song.  I can sing it <input aria-label="text input here" id="input1" size="7" type="text"> day long. </p>',
+});
+
+// rte2.updateData({
+//   text: '<p><input aria-label="math input here" id="input3" size="7" type="math"> little ducks went out one day, over the hills and far away.  Mama duck said <input aria-label="text input here" id="input2" size="50" type="text"> and all of those duckies came a-waddling back.',
+// });
+
+rte2.updateData({
+  text: '<p><input aria-label="math input here" data-type="math" id="input3" size="7" type="text" />&nbsp;little ducks went out one day, over the hills and far away. Mama duck said&nbsp;<input aria-label="text input here" data-type="regular" id="input2" size="50" type="text" />&nbsp;and all of those duckies came a-wadling back.</p>',
+});

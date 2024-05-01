ad497033fc3612e8d86f278d55ef9141f20e0dac
mlibersat <100230347+mlibersat@users.noreply.github.com>
2024-05-01 13:07:18 -0500

    take out readorder

--- Reusable Code/Debugging/show Labels and get Visible objects.js
+++ Reusable Code/Debugging/show Labels and get Visible objects.js
@@ -10,8 +10,8 @@ for (let i = 0, L = objectNames.length; i < L; i++) {
     // ggbApplet.getObjectType(objectNames[i]) === "quadrilateral" ||
     // ggbApplet.getObjectType(objectNames[i]) === "triangle" // only show labels of certain object type
   ) {
-    // console.log(objectNames[i], " visible: ", ggbApplet.getVisible(objectNames[i]));
-    ggbApplet.setLabelVisible(objectNames[i], true);
+    console.log(objectNames[i], " visible: ", ggbApplet.getVisible(objectNames[i]));
+    // ggbApplet.setLabelVisible(objectNames[i], true);
     // console.log("line Style:", ggbApplet.getLineStyle(objectNames[i]));
     // ggbApplet.setColor(objectNames[i], 0, 127, 175);
   }

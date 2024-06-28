f82fdf8f6a892adb2aba03b39921cf96c14081d9
mlibersat <100230347+mlibersat@users.noreply.github.com>
2024-06-28 14:17:28 -0500

    regex tests

--- Reusable Code/Reusable javascript/Regex.js
+++ Reusable Code/Reusable javascript/Regex.js
@@ -15,11 +15,15 @@ const numArray2 = string.search(findNum); // Output: 0
 
 
 // Examples: 
-const re = "5Trapezoid21"
-const regex = /\d+/g
-console.log(re.match(regex)) // ['5', '21']
-console.log(re.search(regex)) // 0
-console.log(re.replace(re.replace(regex, ""),"")) // 521
+const re2 = "5Trapezoid21"
+const regex2 = /\d+/g
+console.log(re2
+.match(regex2)) // ['5', '21']
+console.log(re2
+.search(regex2)) // 0
+console.log(re2
+.replace(re2
+.replace(regex2, ""),"")) // 521
 
 
 // Example2: 
@@ -28,4 +32,35 @@ const regex = /\d+/g
 console.log(re.match(regex)) // ['56', '21']
 console.log(re.match(regex)[0]) // '56'
 console.log(re.search(regex)) // 0
-console.log(re.replace(re.replace(regex, ""),"")) // 5621
+
+console.log(re.replace(re.replace(regex, ""), "")) // 5621
+////////////////////////////////////////////////////////////////////////////////////////
+
+// Find and Replave in VSCode
+
+/* 
+Find and Replace “firstload"
+
+Right-click on the lesson folder in VSCode.
+
+Type slide.on\("firstload", ([\s\S\n]*?;\n*\})\); in the “Find” field.
+
+Choose the RegEx icon at the end of the “Find” field.
+
+Type const { onInit } = didUtils.cD({ components })\n\nonInit($1); in the “Replace” field. 
+
+If necessary, reveal the “Replace” field using the drop down arrow to the left of the “Find” field. 
+
+Ex Before:
+slide.on("firstload", () => {
+  didUtils.disappear(text2, select2, text3);
+});
+
+Ex After:
+const { onInit } = didUtils.cD({ components });
+
+onInit(() => {
+  didUtils.disappear(rte2, select2, rte3);
+});
+*/
+
--- Reusable Code/Reusable javascript/regex for keyboard instructions - line break at 38 chars.js
+++ Reusable Code/Reusable javascript/regex for keyboard instructions - line break at 38 chars.js
@@ -1,3 +1,5 @@
-const someText = "Insert text string here.";
+const someText = "Insert text string here; make it long so you can see the line break at 88 characters.";
 
-someText.replace(/(?![^\n]{1,38}$)([^\n]{1,38})\s/g, "$1\\\\");
+// 88 for KI, use 38 for instructionsText
+const newText = someText.replace(/(?![^\n]{1,38}$)([^\n]{1,88})\s/g, "$1\\\\");
+console.log(newText)

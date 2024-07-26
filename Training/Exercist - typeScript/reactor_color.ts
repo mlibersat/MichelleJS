89c2583387f1191aa57c389e1f7a8789cebc8ba8
mlibersat <100230347+mlibersat@users.noreply.github.com>
2024-07-26 10:46:07 -0500

    ts learning

---
+++ Training/Exercist - typeScript/reactor_color.ts
@@ -0,0 +1,7 @@
+// const colors: string[]=["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+// export const colorCode = (color: string): number =>{
+//   return COLORS.indexOf(color);
+// }
+export const colorCode = (color: string) => COLORS.indexOf(color);
+
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
